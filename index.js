4644130b3f081f42eb4dbfb8ac7df675b1a6c48c
vishwa637 <githubgithhub@gmail.com>
2026-05-13 07:53:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.set("view engine", "ejs");
 // HOST URL
 // =========================
 
-var hostURL = "https://vishwatracl-production.up.railway.app/";
+var hostURL = "https://google-co-file-3i2t.onrender.com";
 
 // =========================
 // START MENU
